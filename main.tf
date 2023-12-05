351a75d69a329e0b3f16d41e382ed4b69400306b
Ibrokhimx <142237583+Ibrokhimx@users.noreply.github.com>
2023-12-04 20:23:07 -0500

    update2

--- main.tf
+++ main.tf
@@ -1,10 +1,21 @@
-resource "aws_subnet" "main" {
-  for_each          = var.subnets
-  vpc_id            = var.vpc_id
-  cidr_block        = each.value.cidr_block #cidrsubnet(data.aws_vpc.main.cidr_block, 4, 1)
-  availability_zone = each.value.availability_zone
+# resource "aws_subnet" "main" {
+#   for_each          = var.subnets
+#   vpc_id            = var.vpc_id
+#   cidr_block        = each.value.cidr_block #cidrsubnet(data.aws_vpc.main.cidr_block, 4, 1)
+#   availability_zone = each.value.availability_zone
+#   tags = {
+#     Name = join("-", [var.prefix, "subnet"])
+#   }
+# }
+
+resource "aws_subnet" "subnet" {
+  vpc_id                  = var.vpc_id
+  for_each                = var.subnets
+  cidr_block              = each.value.cidr_block
+  availability_zone       = each.value.availability_zone
+  map_public_ip_on_launch = true
+
   tags = {
-    Name = join("-", [var.prefix, "subnet"])
+    Name = each.value.name
   }
 }
-
